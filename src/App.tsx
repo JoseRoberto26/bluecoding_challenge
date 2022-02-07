8757d1dbd74526328a676d57c78ba6f4e3eb69c3
José Roberto <jr.lsf26@gmail.com>
2022-02-07 02:01:29 -0300

    chore: Add SampleComponent + sass dependency update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import SampleComponent from './components/SampleComponent/SampleComponent';
 import * as React from 'react';
 const App = () => {
  return (
-  <h3> Welcome to Blue Coding Challenge </h3>
+     <>
+       <h3> Welcome to Blue Coding Challenge </h3>
+       <SampleComponent/>
+     </>
  )
 }
 export default App;
---
+++ src/components/SampleComponent/SampleComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SampleComponentStyle.scss';
+
+const SampleComponent = () => { 
+    return ( 
+        <p className='redFont'>Sample Component</p>
+    )
+}
+
+export default SampleComponent;
